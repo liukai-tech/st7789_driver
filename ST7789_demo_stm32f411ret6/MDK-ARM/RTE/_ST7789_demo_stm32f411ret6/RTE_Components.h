c0d6e76f0c12c21f8127f6e0b0fdd1d916faa731
Caesar <44320838+liukai-tech@users.noreply.github.com>
2021-01-23 08:23:48 -0600

    stm32f411ret6 portable.

---
+++ ST7789_demo_stm32f411ret6/MDK-ARM/RTE/_ST7789_demo_stm32f411ret6/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ST7789_demo_stm32f411ret6' 
+ * Target:  'ST7789_demo_stm32f411ret6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
